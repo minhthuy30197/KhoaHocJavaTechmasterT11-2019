09963269b4d29d5b31f4b436e371d9efec5ed129
minhthuy30197 <minhthuy30197@gmail.com>
2019-11-28 17:16:53 +0700

    first commit lesson 1

---
+++ Buoi1/src/com/company/Hello.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Hello {
+    // Chương trình Java bắt đầu chạy từ hàm main
+    // Class có nhiều nhất một hàm main
+    public static void main(String[] args) {
+        // Sử dụng System.out.println để in ra màn hình
+        System.out.println("Hello World");
+    }
+}
